28f9423ff466d7face91e9b9b9669f23ce7b5329
Mohammed23200 <mgsa112233@gmail.com>
2025-04-25 19:49:28 +0300

    RestFul Comment index

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get('/tacos',(req,res)=>{
-    res.send("get /tacos")
-})
-app.post('/tacos',(req,res)=>{
-    const {meat , qt} = req.body;
-    res.send(`okay , here are tour ${qt} ${meat} tacos`)
-})
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+
+const comments = [
+    {
+        username: 'John',
+        comments: 'lol that is so funny!',
+    },
+    {
+        username: 'Jane',
+        comments: 'I know right?',
+    },
+    {
+        username: 'Jim',
+        comments: 'I am not sure about that',
+    },
+    {
+        username: 'Jack',
+        comments: 'I think it is true',
+    }
+];
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments });
 });
 
+app.get('/tacos', (req, res) => {
+    res.send("get /tacos");
+});
 
+app.post('/tacos', (req, res) => {
+    const { meat, qt } = req.body;
+    res.send(`Okay, here are your ${qt} ${meat} tacos`);
+});
 
-
-
-
-
-
-
-
-// usename 
-// text 
-// Get /allcoments
-// Get /all 
-// GET /ShowmeallcommentsNow
-
-// post /addcomment
-// post /makecomments
-
-// http  root        explain
-// GET /comments -> all comments
-// post /comments -> add a comment
-// GET /comments/:id -> get a specific comment
-// PUT or patch /comments/:id -> update a specific comment
-// DELETE /comments/:id -> delete a specific comment
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
 
